0f54e7df2a2f52794bd7640a541fa649a7c2ac2d
Simon Crouch <iamontheinternet@yahoo.com>
2021-12-07 19:50:03 +0000

    add PW (untested on windows)

--- PlayWrightLinuxNet6/Function1.cs
+++ PlayWrightLinuxNet6/Function1.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Playwright;
+using System.Net;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace PlayWrightLinuxNet6
 {
@@ -17,19 +20,15 @@ namespace PlayWrightLinuxNet6
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            var screenshot = await page.ScreenshotAsync();
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            var result = new OkObjectResult(screenshot);
+            result.ContentTypes.Add("Content-Type: image/png");
+            return result;
         }
     }
 }
